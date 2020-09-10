7854b87e25baf96a051143cfcfe479661389b381
Serkan Türkkolu <serkan@Serkan-MacBook-Pro.local>
2020-09-10 19:42:04 +0300

    sh file edited.

--- posix_build.sh
+++ posix_build.sh
@@ -1,2 +1,15 @@
 g++ -std=c++11 -I./include ./src/udpposix.cpp ./posix/server/main.cpp -o ./posix_server
+if [ $? == 0 ]
+then
+	echo "server executable file was created."
+else
+	echo "server executable file was NOT created!"
+fi
+ 
 g++ -std=c++11 -I./include ./src/udpposix.cpp ./posix/client/main.cpp -o ./posix_client
+if [ $? == 0 ] 
+then 
+	echo "client executable file was created."
+else
+	echo "client executable file was NOT created!"
+fi
